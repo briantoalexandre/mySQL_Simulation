e8af48bfdbb5cd643e881dd52f17da0c9886dedc
alexa <alexandre.brianto29@gmail.com>
2026-08-09 19:27:01 +0200

    added functions, 'methods'. added a help command in userPromptOS

--- dbCommands.c
+++ dbCommands.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 #include <ctype.h>
-#include "cjson\cJSON.h" // have cjon in the include folder
-
-
-// global vars
-//char *databaseFileName = "data/db_default.json";
-char *databaseFileName = "data/db.json";
+#include "cjson\cJSON.h" // git clone https://github.com/DaveGamble/cJSON
 
 // read file function thx chatGPT
 char *read_file(const char *filename) {
@@ -33,6 +29,26 @@ char *read_file(const char *filename) {
     return buffer;
 }
 
+cJSON *read_fileTocJSON(const char *filename) {
+    char *data = read_file(filename);
+    if (!data) {
+        printf("read error\n");
+        return NULL;
+    }
+
+    cJSON *dataP = cJSON_Parse(data); // P for parsed
+    if (!dataP) {
+        printf("parse error\n");
+        free(data);
+        return NULL;
+    }
+    return dataP;
+}
+
+// My Lambdas
+
+#define arrlen(x) (sizeof(x)/sizeof(x[0]))
+
 // My functions
 
 void trim(char *str) {
@@ -53,6 +69,73 @@ void trim(char *str) {
     }
 }
 
+void all_tolower(char *str) {
+    for (int i = 0; str[i]; i++) {
+    str[i] = tolower(str[i]); // Convert each character to lowercase
+    }
+}
+
+void trtolower(char *str) {
+    trim(str);
+    all_tolower(str);
+}
+
+char *join(char **array, const size_t arrSize , const char *sep) {
+    if (arrSize) {
+        int total_length = 1;
+        for (size_t i = 0; i < arrSize; i++) {
+            if (array[i]) {
+                total_length += strlen(array[i]);
+            }
+            if (i < arrSize-1) {
+                total_length += strlen(sep);
+            }
+        }
+
+        char *concat = malloc(total_length);
+        memset(concat, 0, total_length);
+        for (size_t i = 0; i < arrSize; i++) {
+            printf("concat : %s\n", concat);
+            if (i) {
+                strcat(concat, sep);
+            }            
+            strcat(concat, array[i]);
+        }
+        printf("result : %s\n", concat);
+    }
+    return NULL;
+}
+
+char *CJSON_join(cJSON *array, const size_t arrSize , const char *sep) {
+    if (cJSON_IsArray(array) && arrSize) {
+        cJSON *item = NULL;
+        int total_length = 1;
+        for (size_t i = 0; i < arrSize; i++) {
+            item = cJSON_GetArrayItem(array, i);
+            if (cJSON_IsString(item)) {
+                total_length += strlen(item->valuestring);
+            }
+            if (i < arrSize-1) {
+                total_length += strlen(sep);
+            }
+        }
+        
+        char *concat = malloc(total_length);
+        memset(concat, 0, total_length);
+        for (size_t i = 0; i < arrSize; i++) {
+            item = cJSON_GetArrayItem(array, i);
+            if (cJSON_IsString(item)) {
+                if (i) {
+                    strcat(concat, sep);
+                }
+                strcat(concat, item->valuestring);
+            }
+        }   
+        return concat;
+    }
+    return NULL;
+}
+
 void chooser() {}
 
 void addLogin() {}
@@ -61,30 +144,42 @@ void removeLogin() {}
 
 void changeLogin() {}
 
-void t() {}; // test
-
 // classes
+
+struct DictOS {
+    cJSON *data;
+    cJSON *users;
+    cJSON *commands;
+
+    char *fileName;
+};
+
 struct DictDB {
     cJSON *data;
     int uLength; //username max length
     int pLength; //password max length
+
+    char *fileName;
 };
 
 
-
 // DictDB methods
-void init_DictDB(struct DictDB *p, cJSON *data) {
-    if (p) {
-        p->data = data;
+
+// init
+void init_DictDB(struct DictDB *p) {
+
+    p->fileName = "data/db.json";
+    cJSON *datafile = read_fileTocJSON(p->fileName);
+    if (datafile) {
+        p->data = datafile;
 
         int duL = 40; // default username max length value
         int dpL = 40; // default password ..
 
-        cJSON *length = cJSON_GetObjectItem(data, "length");
+        cJSON *length = cJSON_GetObjectItem(p->data, "length");
         if (length) {
 
             cJSON *uL = cJSON_GetObjectItem(length, "username");
-            printf("%s", uL->valuestring);
             if (cJSON_IsNumber(uL)) {
                 p->uLength = uL->valueint;
             }
@@ -103,29 +198,71 @@ void init_DictDB(struct DictDB *p, cJSON *data) {
             }
         }
         else {
-            printf("length key missing in %s", databaseFileName);
+            printf("length key missing in %s", p->fileName);
         }
     }
 }
 
+// DictOS methods
+
+// init
+void init_DictOS(struct DictOS *p) {
+    p->fileName = "data/users.json";
+    cJSON *datafile = read_fileTocJSON(p->fileName);
+    if (datafile) {
+        p->data = datafile;
+        p->users = cJSON_GetObjectItem(p->data, "users");
+        p->commands = cJSON_GetObjectItem(p->data, "commands");
+
+    }
+}
+
+void userPromptOS(struct DictOS *p) {
+    char userInput[100];
+    bool running = true;
+    char *sep = "\n  ";
+    while (running) {
+        printf(":");
+        fgets(userInput, sizeof(userInput), stdin);
+        trtolower(userInput);
+
+        if (strcmp(userInput, "") == 0 || strcmp(userInput, "help") == 0) {
+            printf("Help Menu / commands [%s%s\n]\n", sep, CJSON_join(p->commands, cJSON_GetArraySize(p->commands), sep));
+        }
+    }
+}
+
+char *getPswd(struct DictOS *p) {
+    char userInput[50];
+    printf("username : ");
+    fgets(userInput, sizeof(userInput), stdin);
+    trim(userInput);
+    if ("size : %i",cJSON_GetArraySize(p->users)) {
+        cJSON *dict = NULL;
+        char *user;
+        char *password;
+        cJSON_ArrayForEach(dict, p->users) {
+            user = cJSON_GetObjectItem(dict, "name")->valuestring;
+            if (strcmp(userInput, user) == 0) {
+                password = cJSON_GetObjectItem(dict, "password")->valuestring;
+                return password;
+            }
+        }   
+    }
+    return NULL;
+}
+
 int main() {
-    
-    char *data = read_file(databaseFileName);
-    if (!data) {
-        printf("read error\n");
-        return 1;
-    }
 
-    cJSON *dataP = cJSON_Parse(data);
-    if (!dataP) {
-        printf("parse error\n");
-        free(data);
-        return 1;
-    }
+    //char *t[] = {"hello", "hi"};
 
-    struct DictDB d1;
-    init_DictDB(&d1, dataP);
+    struct DictOS Windows;
+    init_DictOS(&Windows);
 
+    struct DictDB database;
+    init_DictDB(&database);
+
+    userPromptOS(&Windows);
 
     return 0;
 }
